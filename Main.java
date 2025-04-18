94ae33f0a91d63870772fb36176929d149e21764
andyw-59 <148895833+andyw-59@users.noreply.github.com>
2025-04-18 17:01:41 +0000

    4/18/25

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 public class Main {
   public static void main(String[] args) {
+    boolean[][] schedule = new boolean[8][60];
+    AppointmentBook a = new AppointmentBook(schedule);
+    for (int i = 10; i < 15; i++) schedule[1][i] = true;
+    for (int i = 30; i < 45; i++) schedule[1][i] = true;
+    for (int i = 50; i <= 59; i++) schedule[1][i] = true;
+    System.out.println(a.findFreeBlock(2, 15));
+    System.out.println(a.findFreeBlock(2, 9));
+    System.out.println(a.findFreeBlock(2, 20));
 
+    schedule = new boolean[8][60];
+    for (int i = 25; i < 30; i++) schedule[1][i] = true;
+    for (int i = 0; i < 15; i++) schedule[2][i] = true;
+    for (int i = 41; i < 60; i++) schedule[2][i] = true;
+    for (int i = 5; i < 30; i++) schedule[3][i] = true;
+    for (int i = 44; i < 60; i++) schedule[3][i] = true;
+    AppointmentBook b = new AppointmentBook(schedule);
+    b.printPeriod(4);
+    System.out.println(b.makeAppointment(2, 4, 22));
+    b.printPeriod(4);
+    System.out.println(b.makeAppointment(3, 4, 3));
+    System.out.println(b.makeAppointment(2, 4, 30));
   }
 }
